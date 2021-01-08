48ee918b91f9133f11b5f3c558ecd7022790333a
Sandrine <yo.sandra@yahoo.it>
2021-01-08 08:06:47 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "";
-$username = "";
-$password = "";
-$db = "";
+$servername = "ec2-3-90-124-60.compute-1.amazonaws.com";
+$username = "qhxgzwfmxxcfrs";
+$password = "6915d4723a1b1c5ec5c6ee119968083df6a64be211de0623fc250d0c6fe309a3";
+$db = "d43q0g9orllb58";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
